11a0efb6a65d61d3ba25eefcbfea6f68483112dc
longluu2 <longluu2@bu.edu>
2024-09-22 01:29:24 -0400

    heroku

--- cs412/settings.py
+++ cs412/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!hg2(uwyyfgq)q!zvy)aii=sb+y5^o6+3_op^_^3ineszfa6)8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
