fffd7ea4e4001a50bf9f8506a9a7e5ea7d295b97
longluu2 <longluu2@bu.edu>
2024-09-23 02:20:36 -0400

    update quotes app static

--- cs412/settings.py
+++ cs412/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,9 @@ STATIC_URL = "static/"
 
 
 # Define the directory where media files are located
-STATICFILES_DIRS = [BASE_DIR / 'quotes' / 'static',]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'quotes', 'static'),  
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
