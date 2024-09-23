706ed6ceddfce377397626bb2f2806cc6760ec14
longluu2 <longluu2@bu.edu>
2024-09-23 02:45:10 -0400

    update quotes app r add

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("quote", views.quote, name="quote"),  
-    path("show_all/", views.show_all, name="show_all"),
-    path("about/", views.about, name="about"),
+    path(r"", views.home, name="home"),
+    path(r"quote", views.quote, name="quote"),  
+    path(r"show_all/", views.show_all, name="show_all"),
+    path(r"about/", views.about, name="about"),
 ]
