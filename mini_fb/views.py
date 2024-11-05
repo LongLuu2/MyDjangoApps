3af7da61946fd4107c9d78070790ab01d9d60abb
longluu2 <longluu2@bu.edu>
2024-11-04 19:33:43 -0500

    createStatus debug

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -70,7 +70,7 @@ class CreateStatusMessageView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['profile'] = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        context['profile'] = get_object_or_404(Profile, user=self.request.user)
         return context
 
 class UpdateProfileView(UpdateView):
